392fe11c1a28fc7d4d9003784674c7beee639af7
G-Ost <g.ostapczuk@gmail.com>
2021-06-01 22:24:36 +0200

    adjustments

--- bouncy-simulator/src/App.js
+++ bouncy-simulator/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import TileBoard from "./conteners/TileBoard"
-// import Ball from "./components/Ball"
 import React, { useContext } from "react";
 import { BoardContext } from "./BoardContext"
 
 function App() {
 
-  const board = useContext(BoardContext).board;
-  const squareSize = useContext(BoardContext).squareSize;
+  const { board, squareSize } = useContext(BoardContext);
   const boardHorizontalPosition = window.innerWidth / 2 - board[0].length * squareSize / 2 + "px"
   const boardVerticalPosition = window.innerHeight / 2 - board.length * squareSize / 2 + "px";
 
@@ -15,9 +13,7 @@ function App() {
 
   return (
     <div style={{ position: "absolute", left: boardHorizontalPosition, top: boardVerticalPosition }}>
-
       <TileBoard> </TileBoard>
-      {/* <Ball></Ball> */}
     </div >
   );
 }
